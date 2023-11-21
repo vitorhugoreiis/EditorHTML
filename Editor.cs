24223dc8031db1cfc2a353b85467ca17fa282ab8
vitorhugoreiis <vitorhugoreiis@gmail.com>
2023-11-21 09:19:50 -0300

    v1

---
+++ Editor.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+namespace EditorHTML
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Editor mode");
+            Console.WriteLine("-------------------");
+            Start();
+        }
+
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.WriteLine("------------------");
+            Console.WriteLine(" Do you want save the file? ");
+            Viewer.Show(file.ToString());
+        }
+    }
+}
